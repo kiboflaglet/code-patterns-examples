110bc2131be15a1407f8ff35e2cf29912d1a0ab7
kiboflaglet <kiboflaglet@gmail.com>
2025-12-19 15:20:34 +0400

    first

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import { generateRandomLogs, generateRandomNumbers } from "./dataGenerationUtils.js"
-import { benchmark, binaryNumbersSearch, logsBinarySearch, linearNumbersSearch } from "./searchUtils.js"
-
-const numArr = generateRandomNumbers()
-const rand_index = Math.floor(Math.random() * numArr.length)
-const random_num = numArr[rand_index]
-
-const logs = generateRandomLogs() 
-
-benchmark("Linear search", () => { return linearNumbersSearch(numArr, random_num) })
-benchmark("Binary search", () => { return binaryNumbersSearch(numArr, random_num) })
-benchmark("Logs binary search", () => { return logsBinarySearch(logs, 120, 500) })
+console.log('test')
